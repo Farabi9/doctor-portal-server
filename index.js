9122e9fb6cb731e1b8f4af81f60f4e702e30e222
Farabi9 <ifatkhan633@gmail.com>
2022-05-16 23:01:02 +0600

    bokking api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,50 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwls9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+       
+       
+        //services api setup
+        app.get('/service', async(req, res) =>{
+            const query ={};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+        app.post('/booking', async(req, res) =>{
+            const booking = req.body;
+            const query = {treatment: booking.treatment, date:booking.date, patient:booking.patient};
+            const exist = await bookingCollection.findOne(query);
+            if(exist){
+                return res.send({success: false, booking: exist})
+            }
+            const result = await bookingCollection.insertOne(booking);
+           return res.send({success:true, result})
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
     console.log(`Doctor app listening on port ${port}`)
-  })
+})
